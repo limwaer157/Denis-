14d027357c26db55d351d7dd45d01c143ffbe98a
Denis <beznikind@gmail.com>
2023-01-27 22:31:42 +0300

    Определить, присутствует ли в заданном массиве, некоторое число

--- Home_work_seminar4/002/Program.cs
+++ Home_work_seminar4/002/Program.cs
@@ -3,34 +3,16 @@ int[] array = {1,22,34,5,6,7,8,9,0,10};
 
 void MetodRandom(int[] array, int find )
 {
+    int num = 0;
  int len = array.Length;
- int index = 1;
-    
-     if (find == array[index])
-    {
-        Console.WriteLine( find);
-        Console.WriteLine($"{array[index]} ");
-    }
-    else
-    {
-          index = index + 1;
-     }
+ for (int index = 0; index < len; index = index + 1 )
+ {
+    if (array[index] == find )
+    Console.WriteLine("Число "+ $"{ find}" + " есть в массиве"   );
+ 
+   
+
+ }
 
 }
-
-MetodRandom(array , find : 5);
-Console.WriteLine(string.Join("," , array ));
-
-// void MetodFind (int[] array, int find)
-// {
-//  int index = 0;
-//  if (array[index] == find )
-//  {
-//     Console.WriteLine( find);
-//  }
-// else 
-// {
-   
-// }
-// }
-// MetodFind(array , find : 4);
+MetodRandom(array,find : 1 );
