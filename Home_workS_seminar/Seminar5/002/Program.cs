3abcc82d1dd7850417bbfc1217fb8d864c8b5239
Denis <beznikind@gmail.com>
2023-01-30 22:10:21 +0300

    Показать числа Фибоначчи

---
+++ Home_workS_seminar/Seminar5/002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать числа Фибоначчи
+
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+
+Console.Write("Число - ");
+int num = Int32.Parse(Console.ReadLine()) + 1;
+for (int i = 1; i < num; i++)
+{
+  Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
