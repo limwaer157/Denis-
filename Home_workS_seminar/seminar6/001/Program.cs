1de4077417268fc07d7b9f79a194ffafb29aebc9
Denis <beznikind@gmail.com>
2023-02-02 14:56:15 +0300

    Показать двумерный массив размером m×n заполненный вещественными числами

---
+++ Home_workS_seminar/seminar6/001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+
+Console.Write("Введите количество строк - ");
+int m = Int32.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов - ");
+int n = Int32.Parse(Console.ReadLine());
+Random random = new Random();
+void Random(double [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+     for (int j = 0; j < arr.GetLength(1); j++)
+     {
+        arr[i,j] = random.Next(1,10) + random.NextDouble();
+        Console.Write($" {arr [i,j] } ");
+     }
+     Console.WriteLine();
+    }
+
+}
+double[,] arr = new double [m,n];
+Random(arr);
