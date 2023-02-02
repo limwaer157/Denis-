5f33ded8055ad0e97fc57d1948606d4053a7bb1a
Denis <beznikind@gmail.com>
2023-02-02 12:57:26 +0300

    Написать программу копирования массива

---
+++ Home_workS_seminar/Seminar5/003/Program.cs
@@ -0,0 +1,66 @@
+﻿// Написать программу масштабирования фигуры
+
+Console.Clear();
+Console.SetCursorPosition(15, 1);
+Console.WriteLine("-");
+
+Console.Write("Координаты х первой точки фигуры - ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Координаты у второй точки фигуры - ");
+int y = int.Parse(Console.ReadLine());
+Console.Write("Координаты х второй точки фигуры - ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Координаты у второй точки фигуры - ");
+int c = int.Parse(Console.ReadLine());
+Console.Write("Координаты х третьей точки фигуры - ");
+int d = int.Parse(Console.ReadLine());
+Console.Write("Координаты у третьей точки фигуры - ");
+int e = int.Parse(Console.ReadLine());
+Console.Write("Координаты х четвертой точки фигуры - ");
+int f = int.Parse(Console.ReadLine());
+Console.Write("Координаты у четвертой точки фигуры - ");
+int g = int.Parse(Console.ReadLine());
+
+// int xa = 1;
+// int ya = 1;
+// int xb = 1;
+// int yb = 50;
+// int xc = 100;
+// int yc = 50;
+
+Console.SetCursorPosition(x ,y);
+Console.WriteLine("+");
+Console.SetCursorPosition(b,c);
+Console.WriteLine("+");
+Console.SetCursorPosition(d , e);
+Console.WriteLine("+");
+Console.SetCursorPosition(f ,g );
+Console.WriteLine("+");
+
+// int x = xa;
+// int y = xb;
+// int count = 0;
+
+
+// while(count < 10000)
+// {
+//   {
+//     int what = new Random().Next(0, 3);
+//     if(what  == 0)
+//     {
+//         x = (x + xa) / 2;
+//         y = (y + ya) / 2;
+//     }
+//     if (what == 1) 
+//     {
+//     x = (x + xb) / 2;
+//     y = (y + yb) / 2;
+//     }
+//     if(what == 2){
+//     x = (x + xc) / 2;
+//     y = (y + yc) / 2;}
+//    }
+//     Console.SetCursorPosition(x, y);
+//     Console.WriteLine("+");
+//     count = count + 1;
+// }
---
+++ Home_workS_seminar/Seminar5/004/Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу копирования массива
+Console.Write("Введите длинну массива - ");
+int m = Int32.Parse(Console.ReadLine());
+
+void ArrayFill(int[] num)
+{
+ for (int i = 0; i < num.Length; i++)
+ {
+    num[i] = new Random().Next(1,20);
+ }
+
+}
+int[] num = new int[m]; 
+ArrayFill(num);
+Console.WriteLine(string.Join("," , num ));
+
+void CopyArray(int[] num , int [] arr )
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        arr[i] = num[i];
+    }
+} 
+int[] arr = new int[m];
+CopyArray(num,arr);
+Console.WriteLine(string.Join("," , arr ));
