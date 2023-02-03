f5c69aa54f552e3860b73fd581d7e09aa52265d4
Denis <beznikind@gmail.com>
2023-02-03 23:20:18 +0300

    В прямоугольной матрице найти строку с наименьшей суммой элементов.

---
+++ Home_workS_seminar/seminar7/001/Program.cs
@@ -0,0 +1,35 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+
+  Console.Write("Введите количество строк - ");
+  int m = Int32.Parse(Console.ReadLine());
+  Console.Write("Введите количество столбцов - ");
+  int n = Int32.Parse(Console.ReadLine());
+  Random random = new Random();
+
+  void Random(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+{
+        for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j] = random.Next(1,10);
+        Console.Write($" {arr[i,j]}");
+    }
+    Console.WriteLine();
+}
+}
+   int[,] arr = new int[m,n];
+   Random(arr);
+
+   void Gange(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        
+    }
+  }
+
+}
---
+++ Home_workS_seminar/seminar7/003/Program.cs
@@ -0,0 +1,59 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+Console.Write("Введите количество строк - ");
+  int m = Int32.Parse(Console.ReadLine());
+  Console.Write("Введите количество столбцов - ");
+  int n = Int32.Parse(Console.ReadLine());
+  Random random = new Random();
+
+  void Random(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+{
+        for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j] = random.Next(1,10);
+        Console.Write($" {arr[i,j]}");
+    }
+    Console.WriteLine();
+}
+}
+   int[,] arr = new int[m,n];
+   Random(arr);
+   int [] Summ(int[,] arr)
+   {
+    int[] sumArr = new int[arr.GetLength(0)];
+
+     for (int j = 0; j < arr.GetLength(1); j++)
+  {
+   
+       for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sumArr[i] = arr[i,j] + sumArr[i] ;
+    }
+    
+  }
+  return sumArr;
+}
+
+
+int[] sumArr = Summ(arr);
+int min = sumArr[0];
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine($"Сумма {i +1}  строчки  =  {sumArr[i]}");
+}
+
+
+int a = 0;
+for (int i = 0; i < sumArr.Length; i++)
+{
+    
+    if (sumArr[i] < min) 
+    {
+    min = sumArr[i];
+    a = i;
+    }
+}
+
+Console.WriteLine($" Наименьшая сумма чисел  = {min}  которая находится в строчке № {a + 1}");
