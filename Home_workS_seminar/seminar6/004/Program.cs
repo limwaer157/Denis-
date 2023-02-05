8118b64a53e4434fb75d50d010c909381ddf6353
Denis <beznikind@gmail.com>
2023-02-06 00:24:03 +0300

    Написать программу, которая обменивает элементы первой строки и последней строки

--- Home_workS_seminar/seminar6/004/Program.cs
+++ Home_workS_seminar/seminar6/004/Program.cs
@@ -1,62 +1,95 @@
 ﻿// Написать программу, которая обменивает элементы первой строки и последней строки
-
+Console.WriteLine("Hello, World!");
 
 
 Console.Write("Введите количество строк - ");
-int m = Int32.Parse(Console.ReadLine());
+int rows = Int32.Parse(Console.ReadLine());
 Console.Write("Введите количество столбцов - ");
-int n = Int32.Parse(Console.ReadLine());
-Random random = new Random();
-
-void Random(int [,] arr)
+int columns = Int32.Parse(Console.ReadLine());
+void Random(int[,] array1)
 {
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int rows = 0; rows < array1.GetLength(0); rows++)
     {
-        arr[i,j] = random.Next(1,10);
-        Console.Write($" {arr[i,j]}");
-    }
-    Console.WriteLine();
-}
-}
-   int[,] arr = new int[m,n];
-   
-   Random(arr);
-   
-   int[,] next = new int[m,n];
-   
-   Console.WriteLine();
-
-   Arrlast(arr , next);
-
-void Arrlast(int [,] arr , int [,]next )
-{
-    
-    for (int i = arr.GetLength(0) -1 ; i < arr.GetLength(0); i++)
-    {
-        for (int j =  arr.GetLength(1) - n ; j < arr.GetLength(1); j++)
+        for (int columns = 0; columns < array1.GetLength(1); columns++)
         {
-            next[i,j] = arr[i,j] ;
-            Console.Write($" {next[i,j]}");
-         
+            array1[rows,columns] = new Random().Next(1,10);
+            Console.Write(array1 [rows ,columns]);
+            Console.Write(" ");
         }
         Console.WriteLine();
     }
 }
- 
-int[,] first= new int[m,n];
-Arrfirst(arr,first);
-void Arrfirst(int [,] arr , int[,] first)
+
+int[,] array1 = new int[rows,columns];
+int[,] arrayNew = new int[1,columns];
+int rows2 = rows;
+Random(array1);
+
+
+void FirstGangeArray(int[,] array1  , int [,] arrayNew)
 {
-for (int i = 0; i < arr.GetLength(0) - 1; i++)
+   
+    
+for (int rows = 0; rows < array1.GetLength(0); rows++)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int columns = 0; columns < array1.GetLength(1); columns++)
+    {     
+         
+            if (rows ==  0 )
+        {
+           arrayNew[0,columns] = array1[rows,columns];
+           if( columns == array1.GetLength(1) )
+           rows = array1.GetLength(0) +1;
+        }   
+        
+    }    
+}
+}
+
+FirstGangeArray(array1 ,arrayNew);
+
+ int[,] secondArray = new int [1,columns];
+
+void SecondGangeArray(int[,] array1  )
+{
+   
+for (int rows = 0; rows < array1.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < array1.GetLength(1); columns++)
+    {     
+         
+            if (rows ==  array1.GetLength(0) -1 )
+        {
+           secondArray[0,columns] = array1[rows,columns];
+           if( columns == array1.GetLength(1) )
+           rows = array1.GetLength(0) +1;
+        }   
+        
+    }    
+}
+}
+SecondGangeArray(array1);
+
+Console.WriteLine();
+ for (int j = 0; j < secondArray.GetLength(1); j++)
     {
-        first[i,j] = arr[i,j];
-        Console.Write($" {first[i,j]}");
-        Console.Write($" {arr[i,j]  }");
+      Console.Write(secondArray[0,j])  ;
+      Console.Write(" ");
     }
-     Console.WriteLine();
+
+Console.WriteLine();
+for (int i = 1; i < array1.GetLength(0) -1; i++)
+{
+    for (int j = 0; j < array1.GetLength(1); j++)
+    {
+        Console.Write(array1[i,j]);
+        Console.Write(" ");
+           }
+    Console.WriteLine();
 }
-}
+    for (int j = 0; j < arrayNew.GetLength(1); j++)
+    {
+      Console.Write(arrayNew[0,j])  ;
+      Console.Write(" ");
+    }
+Console.WriteLine();
