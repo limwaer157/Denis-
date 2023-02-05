defb7c7e6f0030f2a9e4d37e8be380819c22b6bc
Denis <beznikind@gmail.com>
2023-02-05 23:46:27 +0300

    Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.

--- Home_workS_seminar/seminar7/001/Program.cs
+++ Home_workS_seminar/seminar7/001/Program.cs
@@ -1,220 +1,59 @@
-﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерного массива.
-
-
-// Console.Write("Введите количество строк - ");
-//   int m = Int32.Parse(Console.ReadLine());
-//   Console.Write("Введите количество столбцов - ");
-//   int n = Int32.Parse(Console.ReadLine());
-//   Random random = new Random();
-
-//   void Random(int [,] arr)
-// {
-//     for (int i = 0; i < arr.GetLength(0); i++)
-// {
-//         for (int j = 0; j < arr.GetLength(1); j++)
-//     {
-//         arr[i,j] = random.Next(1,10);
-//         Console.Write($" {arr[i,j]}");
-//     }
-//     Console.WriteLine();
-// }
-// }
-//    int[,] arr = new int[m,n];
-//    Random(arr);
-//    int var = 0;
-//    void SortArr(int[,] arr)
-//    {
-//     int[] sumArr = new int[arr.GetLength(1)];
-
-//      for (int i = 0; i < arr.GetLength(0); i++)
-//   {
-//      for (int j = 0; j < arr.GetLength(1)  -1 ; j++)
-//     {
-//       for (int n = 0; n < arr.GetLength(0) ; n++)
-//       {
-//         for (int m = 0; m < arr.GetLength(1); m++)
-//         {
-//           if (arr[i,j] < arr[i,j + 1 ] )
-//       {
-//         var = arr[i,j];
-//         arr[i,j] = arr[i ,j +1];
-//         arr[i,j+1] = var;
-//       }
-//         }
-         
-//       }
-     
-      
-//     }   
-//   }
-// }
-// SortArr(arr);
-
-// Console.WriteLine();
-
-// Console.WriteLine();
-// for (int i = 0; i < arr.GetLength(0); i++)
-// {
-//   for (int j = 0; j < arr.GetLength(1); j++)
-//   {
-//     Console.Write($" {arr[i,j]}");
-//   }
-//   Console.WriteLine();
-// }
-
-// int[] sumArr = Summ(arr);
-// int min = sumArr[0];
-// for (int i = 0; i < m; i++)
-// {
-//     Console.WriteLine($"Сумма {i +1}  строчки  =  {sumArr[i]}");
-// }
-
-
-// int a = 0;
-// for (int i = 0; i < sumArr.Length; i++)
-// {
-    
-//     if (sumArr[i] < min) 
-//     {
-//     min = sumArr[i];
-//     a = i;
-//     }
-// }
-
-// Console.WriteLine($" Наименьшая сумма чисел  = {min}  которая находится в строчке № {a + 1}");
-
-
-// Написать программу, упорядочивания по убыванию элементы каждой строки двумерного массива.
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
 
 Console.Write("Введите количество строк - ");
-  int m = Int32.Parse(Console.ReadLine());
-  Console.Write("Введите количество столбцов - ");
-  int n = Int32.Parse(Console.ReadLine());
-  Random random = new Random();
-
-  void Random(int [,] arr)
+int rows = Int32.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов - ");
+int columns = Int32.Parse(Console.ReadLine());
+void Random(int[,] array1)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
-{
-        for (int j = 0; j < arr.GetLength(1); j++)
+    for (int rows = 0; rows < array1.GetLength(0); rows++)
     {
-        arr[i,j] = random.Next(1,10);
-        Console.Write($" {arr[i,j]}");
+        for (int columns = 0; columns < array1.GetLength(1); columns++)
+        {
+            array1[rows,columns] = new Random().Next(1,10);
+            Console.Write(array1 [rows ,columns]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array1 = new int[rows,columns];
+int[,] arrayNew = new int[rows,columns];
+
+Random(array1);
+
+void SortArray(int[,] array1 )
+{
+    int var =0;
+    for (int i = 0; i < array1.GetLength(1); i++)
+    {
+for (int rows = 0; rows < array1.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < array1.GetLength(1)-1; columns++)
+    {     
+         
+            if (array1[rows,columns] < array1[rows,columns + 1 ] )
+        {
+           var = array1[rows,columns];
+           array1[rows,columns] = array1[rows,columns + 1 ];
+           array1[rows,columns + 1 ] = var;
+        }   
+    }    
+}
+}
+}
+SortArray(array1); 
+
+Console.WriteLine();
+
+for (int i = 0; i < array1.GetLength(0); i++)
+{
+    for (int j = 0; j < array1.GetLength(1); j++)
+    {
+        Console.Write(array1[i,j]);
+                Console.Write(" ");
+
     }
     Console.WriteLine();
 }
-}
-   int[,] arr = new int[m,n];
-   
-   Random(arr);
-
-int[] sumAr = new int[n];
-
-
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-  
-  for (int j = 0; j <arr.GetLongLength(1); j++)
-  {
-   
-    Summ(arr);
-    sumAr = Summ(arr);
-    Console.WriteLine(string.Join("," , sumAr));
-  }
-  // Console.WriteLine(string.Join("," , sumAr));
-}
-
-
-  //  Summ(arr);
-   
-   
-  //  int[] sumAr = Summ(arr);
-
-   int[] Summ(int[,] arr)
-   {
-//     for (int k = 0; k < arr.GetLength(0); k++)
-// {
-//      for (int j = 0; j < arr.GetLength(1); j++)
-           
-//     {
-//         sumAr[j] = arr[k,j] ;
-//     }
-    {
-   for (int i = 0; i < sumAr.Length; i++)
-            {
-             
-                for (int j = 0; j < sumAr.Length - 1; j++)
-                {
-                    if (sumAr[j] < sumAr[j + 1])
-                    {
-                        int var = sumAr[j];
-                        sumAr[j]= sumAr[j + 1];
-                        sumAr[j + 1] = var;
-                       
-                    }
-                   
-                }
-                
-                  
-                
-                
-            }
-    }  
-     return sumAr;
-    
-}
-  
- 
-
-
-// Console.WriteLine(string.Join("," , sumAr));
-
-// void Print(int [] sumAr )
-// {
-//    for (int i = 0; i < sumAr.Length; i++)
-//             {
-//                 for (int j = 0; j < sumAr.Length - 1; j++)
-//                 {
-//                     if (sumAr[j] < sumAr[j + 1])
-//                     {
-//                         int var = sumAr[j];
-//                         sumAr[j]= sumAr[j + 1];
-//                         sumAr[j + 1] = var;
-
-//                     }
-                    
-//                 }
-//             }
-            
-// }
-
-// for (int i = 0; i < m; i++)
-// {
-//   Print(sumAr);
-//   Console.WriteLine(string.Join("," , sumAr));
-// }
-
-
-//Console.WriteLine(string.Join("," , sumAr));
-
-
-
-// int min = sumArr[0];
-// for (int i = 0; i < m; i++)
-// {
-//     Console.WriteLine($"Сумма {i +1}  строчки  =  {sumArr[i]}");
-// }
-
-
-// int a = 0;
-// for (int i = 0; i < sumArr.Length; i++)
-// {
-    
-//     if (sumArr[i] < min) 
-//     {
-//     min = sumArr[i];
-//     a = i;
-//     }
-// }
-
-// Console.WriteLine($" Наименьшая сумма чисел  = {min}  которая находится в строчке № {a + 1}");
