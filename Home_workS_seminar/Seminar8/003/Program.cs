c0c4def4f4bc102953ce26c9a1454650eaaef84f
Denis <beznikind@gmail.com>
2023-02-09 23:19:15 +0300

    Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента

--- Home_workS_seminar/Seminar8/003/Program.cs
+++ Home_workS_seminar/Seminar8/003/Program.cs
@@ -12,16 +12,20 @@ Console.Write("Введите количество по оси х - ");
 
 void FirstMatrix(int[,,] first)
 {
+    int n = 10;
     for (int i = 0; i < first.GetLength(0); i++)
     {
+        
         for (int j = 0; j < first.GetLength(1); j++)
         {
+            
             for (int k = 0; k < first.GetLength(2); k++)
             {
 
-                   first[i,j,k] = first[i,j,k] + i +10 ;
+                   first[i,j,k] = n + 1 ;
                    Console.Write(first[i,j,k] + $"(x-{i} y-{j} z-{k})");
                    Console.Write(" ");
+                   n = n+ 1;
             }
          Console.WriteLine();
         }
